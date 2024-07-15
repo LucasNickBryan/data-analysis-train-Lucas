3c7cdf7d4648ba183e591951a5ff573bf90b37bb
LucasNickBryan <rlucasnickbryan@gmail.com>
2024-07-15 19:08:32 +0300

    Feat: Calculate average number of unique bidders per auction in each of the three separate time periods

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the data
-df = pd.read_csv('./datas/bids.csv')
-
-# 1. Get the number of unique IPs per auction
-auctionIpCounts = df.groupby('auction')['ip'].nunique().reset_index()
-print(auctionIpCounts)
